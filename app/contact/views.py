3ea2b46a02b13aa8fed7c05c22bb273865fcec30
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-10 16:26:16 +0300

    Ajout de contact view et html

--- app/contact/views.py
+++ app/contact/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Contact
+from django.views.generic.list import ListView
+
 
 # Create your views here.
+def create_contact(request):
+    name = request.POST.get('name')
+    phone = request.POST.get('phone')
+
+    contact = Contact.objects.create(name=name, phone=phone)
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contact/contact-list.html', context)
+
+
+class ContactList(ListView):
+    template_name = 'contact/contact.html'
+    model = Contact
+    context_object_name = 'contacts'
