96a793c1f7e437e94e9aeb75d1fba5d7a7043e7d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-10 17:25:02 +0300

    Ajout de method delete

--- app/contact/views.py
+++ app/contact/views.py
@@ -17,6 +17,16 @@ def create_contact(request):
     return render(request, 'contact/contact-list.html', context)
 
 
+def delete_contact(request, pk):
+    contact_id = Contact.objects.get(id=pk)
+    contact_id.delete()
+    contacts = Contact.objects.all()
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contact/contact-list.html', context)
+
+
 class ContactList(ListView):
     template_name = 'contact/contact.html'
     model = Contact
